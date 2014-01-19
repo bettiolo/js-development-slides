5d8e5dc80b4d04236c683972a489932763099ce0
Marco Bettiolo <marco@bettiolo.it>
2014-01-19 16:31:40 +0000

    Example for classical javascript inheritance

---
+++ classic-objects/pint.js
@@ -0,0 +1,40 @@
+// Pub is our namespace
+window.Pub = (function (Pub) {
+	'use strict';
+
+	var Pint = function () { // Constructor
+		this._quantity = 20; // private by convention
+	};
+
+	// private by convention
+	Pint.prototype._consume = function (quantityToConsume) {
+		if (this._quantity <= 0) {
+			return;
+		}
+		if (this._quantity > quantityToConsume) {
+			this._quantity -= quantityToConsume;
+		} else {
+			this._quantity = 0;
+		}
+	};
+
+	Pint.prototype.drink = function () {
+		this._consume(1);
+	};
+
+	Pint.prototype.quaff = function () {
+		this._consume(4);
+	};
+
+	Pint.prototype.downInOne = function () {
+		this._consume(this._quantity);
+	};
+
+	Pint.prototype.getQuantity = function () {
+		return this._quantity;
+	};
+
+	Pub.Pint = Pint;
+	return Pub;
+
+}(window.Pub || {}));
---
+++ classic-objects/pint.spec.js
@@ -0,0 +1,58 @@
+/* global describe, beforeEach, it, expect */
+describe('Pint', function () {
+	'use strict';
+
+	var pint;
+
+	beforeEach(function () {
+		pint = new window.Pub.Pint();
+	});
+
+	it('Contains 20 fl. oz. of beer', function () {
+		expect(pint.getQuantity()).toEqual(20);
+	});
+
+});
+
+describe('Pint Customer', function () {
+	'use strict';
+
+	var pint;
+
+	beforeEach(function () {
+		pint = new window.Pub.Pint();
+	});
+	
+	it('Drinks, 1 fl. oz. is consumed', function () {
+		pint.drink();
+		expect(pint.getQuantity()).toEqual(19);
+	});
+
+	it('Quaffs, 4 fl. oz. are consumed', function () {
+		pint.quaff();
+		expect(pint.getQuantity()).toEqual(16);
+	});
+
+	it('Drinks and then downs in one, there remaining beer is consumed', function () {
+		pint.drink();
+		pint.downInOne();
+		expect(pint.getQuantity()).toEqual(0);
+	});
+
+	it('Cannot drink more beer than the one available in the glass', function () {
+		pint.drink();
+		pint.quaff();
+		pint.quaff();
+		pint.quaff();
+		pint.quaff();
+		pint.quaff();
+		expect(pint.getQuantity()).toEqual(0);
+	});
+
+	it('Can drink and quaff', function () {
+		pint.drink();
+		pint.quaff();
+		expect(pint.getQuantity()).toEqual(15);
+	});
+
+});
