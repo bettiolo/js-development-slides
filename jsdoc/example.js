053fa3a90ca858d547c729f8f2943196bea8ee06
Marco Bettiolo <marco@bettiolo.it>
2014-01-19 22:43:32 +0000

    Updated JsDoc example

--- jsdoc/example.js
+++ jsdoc/example.js
@@ -1,83 +1,80 @@
-var Glass = function () {
+window.Pub2 = (function (Pub) {
 	'use strict';
 
 	/**
 	 * @constructor
-	 * @param {number} capacity The capacity of the glass in oz.
+	 * @param {number} quantity The quantity of liquid in the glass in fl. oz.
 	 * @abstract
 	 */
-	function Glass(capacity) {
-		this.quantity = 0; // Instance property
-		this.capacity = capacity; // Instance property
-	}
+	var Glass = function (quantity) { // Base class constructor
+		/** @private */
+		this._quantity = quantity; // Private by convention
+	};
 
-	// Ensure the constructor is correct
-	Glass.prototype.constructor = Glass;
-
-	/** @param {number} consumedQuantity How much has been consumed in oz. */
-	Glass.prototype.consume = function (consumedQuantity) {
-		if (this.quantity > 0) {
-			this.quantity -= consumedQuantity;
+	/**
+	 * @param {number} quantityToConsume How much liquid to consumed in fl. oz.
+	 * @private
+	 */
+	Glass.prototype._consume = function (quantityToConsume) {
+		if (this._quantity <= 0) {
+			return;
+		}
+		if (this._quantity > quantityToConsume) {
+			this._quantity -= quantityToConsume;
+		} else {
+			this._quantity = 0;
 		}
 	};
 
 	Glass.prototype.drink = function () {
-		this.consume(1);
+		this._consume(1);
 	};
 
-	Glass.prototype.quaff =	function () {
-		this.consume(4);
+	Glass.prototype.quaff = function () {
+		this._consume(4);
 	};
 
 	Glass.prototype.downInOne = function () {
-		this.consume(this.quantity);
+		this._consume(this._quantity);
 	};
 
-	// Return the constructor
-	return Glass;
-
-}(); // Immediately-Invoked Function Expression (IIFE)
-
-var Pint = function () {
-	'use strict';
+	/** @returns {number} Quantity in fl. oz. */
+	Glass.prototype.getQuantity = function () {
+		return this._quantity;
+	};
 
 	/**
 	 * @constructor
 	 * @extends {Glass}
 	 */
+		// The inheriting class constructor
 	function Pint() {
-		Glass.call(this, 20); // Pint size in fl. oz.
-		this.quantity = 20; // Pint size in fl. oz.
+		// Call the base class constructor
+		Glass.call(this, 20);
 	}
 
-	// Inherit the base class
+	// Inherit Glass
+	// Supported by Chrome: 5, Firefox 4.0, IE 9, Opera 11.60 and Safari 5
+	// es5-shim for legacy support
 	Pint.prototype = Object.create(Glass.prototype);
-
-	// Ensure the constructor is correct
+	// Correct the constructor pointer because it points to Glass
 	Pint.prototype.constructor = Pint;
 
-	return Pint;
-}();
+	Pub.Pint = Pint;
 
-var p = new Pint();
-// p.capacity; // intellisense works with jsdoc but does not work without
-p.consume(); // Gives parameter intellisense
-
-var HalfPint = function () {
-	'use strict';
-
-	/**
-	 * @constructor
-	 * @extends {Glass}
-	 */
+//	/**
+//	 * @constructor
+//	 * @extends {Glass}
+//	 */
 	function HalfPint() {
-		Glass.call(this, 10); // Half Pint size in fl. oz.
-		this.quantity = 10; // Half Pint size in fl. oz.
+		Glass.call(this, 10);
 	}
 
 	HalfPint.prototype = Object.create(Glass.prototype);
-
 	HalfPint.prototype.constructor = HalfPint;
 
-	return HalfPint;
-}();
+	Pub.HalfPint = HalfPint;
+
+	return Pub;
+
+}(window.Pub2 || {}));
